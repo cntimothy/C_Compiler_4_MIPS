118c18168b17b05cf55eeee4f09ebd39327efe55
cntimothy <cntimothy1990@gmail.com>
2013-11-01 22:18:02 +0800

    ENH:add MainProject to solution

---
+++ MainProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MIPS246.Compiler.MainProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
