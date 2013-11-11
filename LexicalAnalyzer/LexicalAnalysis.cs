13d4b4f2ac204ee04cd8151a565a339098eab068
cntimothy <cntimothy1990@gmail.com>
2013-11-11 20:59:33 +0800

    ENH:support remove lines start with '//'

--- LexicalAnalyzer/LexicalAnalysis.cs
+++ LexicalAnalyzer/LexicalAnalysis.cs
@@ -38,7 +38,7 @@ namespace MIPS246.Compiler.LexicalAnalyzer
 
         #region Private Method
         /// <summary>
-        /// 将指定源文件去掉空行和#行读到List中
+        /// 将指定源文件去掉空行，注释行和#行读到List中
         /// </summary>
         /// <param name="filePath"></param>
         /// <returns></returns>
@@ -53,7 +53,7 @@ namespace MIPS246.Compiler.LexicalAnalyzer
                     while ((line = sr.ReadLine()) != null)
                     {
                         line = line.Trim();
-                        if (line.StartsWith("#"))
+                        if (line.StartsWith("#") || line.StartsWith("//"))
                         {
                             continue;
                         }
